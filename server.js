3cf36b4fc9cf2bd8bfdb9a3a675182a5d4057e2f
Jeff Johnston <jeffwjohn@yahoo.com>
2020-10-21 20:30:20 -0500

    connect to jawsdb

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+// This uses Heroku's process.env.PORT value when deployed and 3001 when run locally. Having a dynamic port number is important, because it is very unlikely that the port number you hardcode (e.g., 3001) would be the port Heroku runs your app on.
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
