ac5b2610ed3f6492905a4d8114d7ed59f0dc068c
Jeff Johnston <jeffwjohn@yahoo.com>
2020-10-25 16:29:50 -0500

    fix comment route

--- controllers/index.js
+++ controllers/index.js
@@ -4,6 +4,10 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api');
 
+const homeRoutes = require('./home-routes.js');
+
+router.use('/', homeRoutes);
+
 router.use('/api', apiRoutes);
 
 //if we make a request to any endpoint that doesn't exist, we'll receive a 404 error indicating we have requested an incorrect resource:
